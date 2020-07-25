fe79a1630efc9dc5d1243573740427a58fd19ce4
Moriah_Obaje <mobaje20@gmail.com>
2020-07-25 12:29:41 +0100

    Web Devop Training

---
+++ Assingments/script.js
@@ -0,0 +1,28 @@
+var name = "Moriah Obaje" 
+var favFood = "Every food is my favourite, i do not discriminate (Fried Rice and Chicken)"
+var favMonth = "June"
+
+console.log(name)
+console.log(favFood)
+console.log(favMonth)
+
+// Assignment 
+// Number 1: Profit Calculator
+var percentage = 60/100; 
+var totalSale = 4600000;
+profit = totalSale * percentage;
+console.log("The company made a profit of " + profit);
+
+// Number 2: Land Calculator
+var acre = 43560; 
+var tract = 389767; 
+var acres = tract/acre;
+console.log(tract + " is equivalent to " + acres );
+
+for(i =0; i <= 10; i++ )
+{
+    for(i == 10; i =10; i--)
+    {
+        console.log(i);
+    }
+}
